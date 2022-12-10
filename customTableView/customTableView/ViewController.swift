1ecdb7630ce21c282a9f12437f0a35b64037f326
Emre Özbağdatlı <74432853+EmreOzbagdatli@users.noreply.github.com>
2022-12-11 00:18:01 +0300

    .

---
+++ customTableView/customTableView/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  customTableView
+//
+//  Created by Emre Özbağdatlı on 11.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+
+    }
+
+
+}
+
